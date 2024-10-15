f0f25d1ffcd24628579bb20938aee97f9c849083
wangshangwen <shangwenwang20@gmail.com>
2024-10-15 16:53:31 +0800

    chore: transfer eth by truly sender

--- core/claim.go
+++ core/claim.go
@@ -3,43 +3,40 @@ package core
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 var UserCookies = []*http.Cookie{}
 var endpoint = "http://localhost:8595/sendtx"
 
-var str = "{\"data\":[{\"hash\":\"0xa7dd50bf3026c8d26f492faa105e99b7fceb6ac8041bce7d26f5a490fa783574\",\"amount\":\"1000000000000\",\"token\":\"ETH\",\"chain\":\"ethereum\"}],\"err\":0,\"message\":\"ok\"}"
-
 func ClaimToken[T any](chain string, toAddress string) (T, error) {
 	var result T
 
-	// req := &TestnetFaucet{
-	// 	Chain:     chain,
-	// 	ToAddress: toAddress,
-	// }
+	req := &TestnetFaucet{
+		Chain:     chain,
+		ToAddress: toAddress,
+	}
 
-	// bytes, err := json.Marshal(req)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return result, err
-	// }
+	bytes, err := json.Marshal(req)
+	if err != nil {
+		fmt.Println(err)
+		return result, err
+	}
 
-	// resp, err := req.PostWithCookie(endpoint, bytes, UserCookies)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return result, err
-	// }
+	resp, err := req.PostWithCookie(endpoint, bytes, UserCookies)
+	if err != nil {
+		fmt.Println(err)
+		return result, err
+	}
 
-	// defer resp.Body.Close()
+	defer resp.Body.Close()
 
-	// bytes, err = io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return result, err
-	// }
-
-	bytes := []byte(str)
+	bytes, err = io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return result, err
+	}
 
 	if err := json.Unmarshal(bytes, &result); err != nil {
 		fmt.Println(err)
